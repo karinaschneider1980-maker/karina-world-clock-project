6594cee49cf684a68dd572a72ba87affd2ec5e05
karinaschneider1980-maker <karinaschneider1980@gmail.com>
2026-04-04 11:14:56 +0200

    insert JS and updated HTML

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,32 @@
+function updateTime() {
+  // usa
+  let usaElement = document.querySelector("#usa")
+  let usaDateElement = usaElement.querySelector(".date")
+  let usaTimeElement = usaElement.querySelector(".time")
+  let usaTime = moment().tz("America/New_York")
 
+  usaDateElement.innerHTML = usaTime.format("dd MMMM Do YYYY")
+  usaTimeElement.innerHTML = usaTime.format("h:mm:ss [<small>]A[</small>]")
+
+  // europe
+  let europeElement = document.querySelector("#europe")
+  let europeDateElement = europeElement.querySelector(".date")
+  let europeTimeElement = europeElement.querySelector(".time")
+  let europeTime = moment().tz("Europe/Amsterdam")
+
+  europeDateElement.innerHTML = europeTime.format("dd MMMM Do YYYY")
+  europeTimeElement.innerHTML = europeTime.format(
+    "h:mm:ss [<small>]A[</small>]",
+  )
+
+  // asia
+  let asiaElement = document.querySelector("#asia")
+  let asiaDateElement = asiaElement.querySelector(".date")
+  let asiaTimeElement = asiaElement.querySelector(".time")
+  let asiaTime = moment().tz("Asia/Seoul")
+
+  asiaDateElement.innerHTML = asiaTime.format("dd MMMM Do YYYY")
+  asiaTimeElement.innerHTML = asiaTime.format("h:mm:ss [<small>]A[</small>]")
+}
+updateTime()
+setInterval(updateTime, 1000)
