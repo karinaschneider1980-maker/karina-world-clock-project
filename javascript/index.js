359fde5a7af6bc58bb287175dd6542e63a7c48fc
karinaschneider1980-maker <karinaschneider1980@gmail.com>
2026-04-04 19:26:43 +0200

    made changes html and css and updated js

--- javascript/index.js
+++ javascript/index.js
@@ -1,32 +1,64 @@
+let cityInterval = null
+
 function updateTime() {
-  // usa
   let usaElement = document.querySelector("#usa")
-  let usaDateElement = usaElement.querySelector(".date")
-  let usaTimeElement = usaElement.querySelector(".time")
-  let usaTime = moment().tz("America/New_York")
+  if (usaElement) {
+    let usaDateElement = usaElement.querySelector(".date")
+    let usaTimeElement = usaElement.querySelector(".time")
+    let usaTime = moment().tz("America/New_York")
+    usaDateElement.innerHTML = usaTime.format("dd MMMM Do YYYY")
+    usaTimeElement.innerHTML = usaTime.format("h:mm:ss [<small>]A[</small>]")
+  }
 
-  usaDateElement.innerHTML = usaTime.format("dd MMMM Do YYYY")
-  usaTimeElement.innerHTML = usaTime.format("h:mm:ss [<small>]A[</small>]")
-
-  // europe
   let europeElement = document.querySelector("#europe")
-  let europeDateElement = europeElement.querySelector(".date")
-  let europeTimeElement = europeElement.querySelector(".time")
-  let europeTime = moment().tz("Europe/Amsterdam")
+  if (europeElement) {
+    let europeDateElement = europeElement.querySelector(".date")
+    let europeTimeElement = europeElement.querySelector(".time")
+    let europeTime = moment().tz("Europe/Amsterdam")
+    europeDateElement.innerHTML = europeTime.format("dd MMMM Do YYYY")
+    europeTimeElement.innerHTML = europeTime.format(
+      "h:mm:ss [<small>]A[</small>]",
+    )
+  }
 
-  europeDateElement.innerHTML = europeTime.format("dd MMMM Do YYYY")
-  europeTimeElement.innerHTML = europeTime.format(
-    "h:mm:ss [<small>]A[</small>]",
-  )
-
-  // asia
   let asiaElement = document.querySelector("#asia")
-  let asiaDateElement = asiaElement.querySelector(".date")
-  let asiaTimeElement = asiaElement.querySelector(".time")
-  let asiaTime = moment().tz("Asia/Seoul")
-
-  asiaDateElement.innerHTML = asiaTime.format("dd MMMM Do YYYY")
-  asiaTimeElement.innerHTML = asiaTime.format("h:mm:ss [<small>]A[</small>]")
+  if (asiaElement) {
+    let asiaDateElement = asiaElement.querySelector(".date")
+    let asiaTimeElement = asiaElement.querySelector(".time")
+    let asiaTime = moment().tz("Asia/Seoul")
+    asiaDateElement.innerHTML = asiaTime.format("dd MMMM Do YYYY")
+    asiaTimeElement.innerHTML = asiaTime.format("h:mm:ss [<small>]A[</small>]")
+  }
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value
+
+  if (cityInterval) {
+    clearInterval(cityInterval)
+  }
+
+  function showCityTime() {
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1]
+    let cityTime = moment().tz(cityTimeZone)
+    let citiesElement = document.querySelector("#cities")
+    citiesElement.innerHTML = `
+      <div class="city">
+        <div>
+          <h4>${cityName}</h4>
+          <div class="date">${cityTime.format("dd MMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+      </div>
+    `
+  }
+
+  showCityTime()
+  cityInterval = setInterval(showCityTime, 1000)
+}
+
 updateTime()
 setInterval(updateTime, 1000)
+
+let citiesSelectElement = document.querySelector("#city")
+citiesSelectElement.addEventListener("change", updateCity)
